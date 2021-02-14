0a98f068e761ace337f3926f0590c8c228849e3e
HeptaDecane <sooraj1999vs@gmail.com>
2021-02-15 03:15:59 +0530

    Pass 1

--- A01/src/xyz/heptadecane/Main.java
+++ A01/src/xyz/heptadecane/Main.java
@@ -1,4 +1,13 @@
 package xyz.heptadecane;
 
 public class Main {
+    public static void main(String[] args) {
+
+        PassOne passOne = new PassOne("input.asm");
+        try {
+            passOne.parseFile();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }

d36a2c52a003a2581dfec00162669431fbea739a
HeptaDecane <sooraj1999vs@gmail.com>
2021-03-06 13:43:25 +0530

    MacroProcessor Pass I

--- A03/src/Main.java
+++ A03/src/Main.java
@@ -1,9 +1,53 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+        BufferedReader bufferedReader = null;
+        int index;
+        String file,line;
         MacroProcessor macroProcessor = new MacroProcessor();
-        macroProcessor.setFile("input.txt");
+
+        System.out.print("File Path: ");
+        file = scanner.nextLine();
+        macroProcessor.setFile(file);
+
         try{
             macroProcessor.passOne();
+
+            bufferedReader = new BufferedReader(new FileReader("MNT.txt"));
+            System.out.println("\nMacro Name Table:");
+            while ((line=bufferedReader.readLine())!=null)
+                System.out.println(line);
+            bufferedReader.close();
+
+            System.out.println();
+
+            bufferedReader = new BufferedReader(new FileReader("MDT.txt"));
+            System.out.println("\nMacro Definition Table:");
+            while ((line=bufferedReader.readLine())!=null)
+                System.out.println(line);
+            bufferedReader.close();
+
+            System.out.println();
+
+            bufferedReader = new BufferedReader(new FileReader("DEFAULT_ARGUMENTS.txt"));
+            System.out.println("\nDefault Arguments:");
+            while ((line=bufferedReader.readLine())!=null)
+                System.out.println(line);
+            bufferedReader.close();
+
+            System.out.println();
+
+            bufferedReader = new BufferedReader(new FileReader("CODE.txt"));
+            System.out.println("\nCODE:");
+            while ((line=bufferedReader.readLine())!=null)
+                System.out.println(line);
+            bufferedReader.close();
+
         }catch (Exception e){
             System.out.println(e);
         }
